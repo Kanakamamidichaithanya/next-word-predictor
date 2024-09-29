64648651144375d834514fc243d7da8b6e69af4d
kanakamamidi chaithanya <kanakamamidichaithanya>
2024-09-30 03:02:45 +0530

    modelling using tensor flow

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
 import numpy as np
 import time
 import sys
-import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import PhotoImage
+import urllib.request
 
 
 '''step 1: Data aquisation'''
@@ -30,3 +38,29 @@ An adventure awaited just beyond the horizon.
 
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts([faqs])
+
+
+'''step 2: data cleaning'''
+input_sequences = []
+for sentence in faqs.split('\n'):
+    tokenized_sentence = tokenizer.texts_to_sequences([sentence])[0]
+    for i in range(1, len(tokenized_sentence)):
+        input_sequences.append(tokenized_sentence[:i+1])
+
+'''step 3: Data preprocessing'''
+max_len = max([len(x) for x in input_sequences])
+padded_input_sequences = pad_sequences(input_sequences, maxlen=max_len, padding='pre')
+
+X = padded_input_sequences[:, :-1]
+y = padded_input_sequences[:, -1]
+
+y = to_categorical(y, num_classes=len(tokenizer.word_index) + 1)
+
+'''step 4: modelling'''
+model = Sequential()
+model.add(Embedding(len(tokenizer.word_index) + 1, 100))
+model.add(LSTM(150, return_sequences=True))
+model.add(LSTM(150))
+model.add(Dense(len(tokenizer.word_index) + 1, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(X, y, epochs=100)
